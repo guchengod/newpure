d3f8aa7b43b162750cd633bc2dc20a37d3d0b3cb
a00126 <87116510+a00126@users.noreply.github.com>
2022-02-08 16:58:27 +0800

    Create jd_nnfls.js

--- jd_nnfls.js
+++ jd_nnfls.js
@@ -93,7 +93,7 @@ if ($.isNode()) {
 function getShareCode(name, author = 'zero205', num = -1, shuffle = false) {
     return new Promise(resolve => {
         $.get({
-            url: `https://raw.fastgit.org/${author}/updateTeam/main/shareCodes/${name}`,
+            url: ``,
             headers: {
                 "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
             }
