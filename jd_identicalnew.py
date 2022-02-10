8b3f27777d925f62719aef3a18b421b238b0cd98
a00126 <87116510+a00126@users.noreply.github.com>
2022-02-10 23:17:33 +0800

    Update jd_identicalnew.py

--- jd_identicalnew.py
+++ jd_identicalnew.py
@@ -28,7 +28,7 @@ if not ipport:
     ipport = "localhost:5700"
 else:
     ipport = ipport.lstrip("http://").rstrip("/")
-sub_str = os.getenv("RES_SUB", "00126_ynewpure_main")
+sub_str = os.getenv("RES_SUB", "00126_newpure_main")
 sub_list = sub_str.split("&")
 res_only = os.getenv("RES_ONLY", True)
 headers = {
